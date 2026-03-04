772d8dbdae0c9ac0c68252ee5e846788d83a298e
estebangastaudo-lgtm <estebangastaudo@gmail.com>
2026-03-04 19:31:39 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "cronometro-enduro-autoQR-v1-1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-512.png", "./apple-touch-icon.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : Promise.resolve()))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
